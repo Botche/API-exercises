9f0f0967aaa19796aba51d68bed0e2be1f8b1072
Gabriel <47752724+Botche@users.noreply.github.com>
2021-10-03 10:49:51 +0300

    Create simple API

---
+++ first-api/Test/Controllers/WeatherForecastController.cs
@@ -0,0 +1,94 @@
+﻿namespace Test.Controllers
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	using Microsoft.AspNetCore.Mvc;
+
+	using Test.Database;
+
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private readonly ApplicationDbContext dbContext;
+
+		public WeatherForecastController(ApplicationDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		[HttpGet]
+		public IEnumerable<WeatherForecast> Get()
+		{
+			List<WeatherForecast> weatherForecasts = this.dbContext
+				.WeatherForecasts
+				.ToList();
+
+			return weatherForecasts;
+		}
+
+		[HttpGet]
+		[Route("{id?}")]
+		public WeatherForecast Get(int id)
+		{
+			WeatherForecast weatherForecast = this.dbContext
+				.WeatherForecasts
+				.Where(wf => wf.Id == id)
+				.SingleOrDefault();
+
+			return weatherForecast;
+		}
+
+		[HttpPost]
+		public bool Post(WeatherForecast model)
+		{
+			this.dbContext
+				.WeatherForecasts
+				.Add(model);
+
+			this.dbContext.SaveChanges();
+
+			return true;
+		}
+
+		[HttpDelete]
+		[Route("{id?}")]
+		public bool Delete(int id)
+		{
+			WeatherForecast weatherForecast = this.dbContext
+				.WeatherForecasts
+				.Find(id);
+
+			this.dbContext
+				.WeatherForecasts
+				.Remove(weatherForecast);
+
+			this.dbContext.SaveChanges();
+
+			return true;
+		}
+
+		[HttpPut]
+		public bool Put(WeatherForecast model)
+		{
+			WeatherForecast weatherForecast = this.dbContext
+				.WeatherForecasts
+				.Find(model.Id);
+
+			weatherForecast.Date = model.Date;
+			weatherForecast.TemperatureC = model.TemperatureC;
+			weatherForecast.Summary = model.Summary;
+
+			this.dbContext
+				.WeatherForecasts
+				.Update(weatherForecast);
+
+			this.dbContext
+				.SaveChanges();
+
+			return true;
+		}
+	}
+}
---
+++ first-api/Test/Database/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Test.Database
+{
+	using Microsoft.EntityFrameworkCore;
+
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+
+			optionsBuilder.UseSqlServer("Server=.;Database=PeopleAPITest;Integrated Security = true;");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
